cadbe54eb9ea9ca7c8d53a92abc98ab878cda432
Daggim 99 <programmerdaggim99@gmail.com>
2023-01-24 16:11:45 +0300

    fix

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://ecommerce-cgim.onrender.com/products?category=${cat}`
+            : "https://ecommerce-cgim.onrender.com/products"
         );
         setProducts(res.data);
       } catch (err) {}
